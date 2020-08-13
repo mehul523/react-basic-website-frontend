a54e10032d72e0190d5ea99e94aaa34e2c306e35
mehulchotaliya <mehulchotaliya523@gmail.com>
2020-08-13 18:06:05 +0530

    Updates

---
+++ precache-manifest.3d5ae54070afe57d1a85549060b768db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d810c66165351ae5a6b69c43ab4a9a1",
+    "url": "/react-basic-website-frontend/index.html"
+  },
+  {
+    "revision": "66c268459afe21a9a119",
+    "url": "/react-basic-website-frontend/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "69b8355448dbd4704389",
+    "url": "/react-basic-website-frontend/static/css/main.ed3145a2.chunk.css"
+  },
+  {
+    "revision": "66c268459afe21a9a119",
+    "url": "/react-basic-website-frontend/static/js/2.7fdfb384.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-basic-website-frontend/static/js/2.7fdfb384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b8355448dbd4704389",
+    "url": "/react-basic-website-frontend/static/js/main.301b3c32.chunk.js"
+  },
+  {
+    "revision": "0716b6f067573f41a0b4",
+    "url": "/react-basic-website-frontend/static/js/runtime-main.c8468a22.js"
+  },
+  {
+    "revision": "e5c4ba08c1a82ef1dc0c72885b61ca05",
+    "url": "/react-basic-website-frontend/static/media/img.e5c4ba08.svg"
+  },
+  {
+    "revision": "d043e42ecbf27a0737a6e341d33f2316",
+    "url": "/react-basic-website-frontend/static/media/img2.d043e42e.svg"
+  },
+  {
+    "revision": "132efd7aea2f5f94844b6b54adbc747e",
+    "url": "/react-basic-website-frontend/static/media/me4.132efd7a.jpg"
+  },
+  {
+    "revision": "5726b79af49c9ee15fb9b505c2347aa3",
+    "url": "/react-basic-website-frontend/static/media/me6.5726b79a.jpg"
+  }
+]);
